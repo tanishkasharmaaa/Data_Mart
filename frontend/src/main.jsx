e28011f11dc0aa0a3b1fb900d2500bbc5f0fb4e4
Tanishka Sharma <anjanasharma8448766174@gmail.com>
2026-03-08 22:37:11 +0530

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, extendTheme, ColorModeScript } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 
@@ -71,9 +72,11 @@ const theme = extendTheme({
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <BrowserRouter>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
